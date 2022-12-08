4c4e820918ca662e5d7a1c5cd452c149f8050bb5
ZihanWang314 <ZihanWang314>
2022-12-08 10:29:47 +0800

    add plots

--- main.py
+++ main.py
@@ -54,18 +54,18 @@ root_folder = r".."
 
 batch_size = args.batch_size
 
-if args.data_augmentation:
-    trainset = torch.load(os.path.join(root_folder,'train_dataset_augmented.pt'))
-else:
-    trainset = torch.load(os.path.join(root_folder,'train_dataset.pt'))
-testset = torch.load(os.path.join(root_folder, 'dev_dataset.pt'))
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                          shuffle=True, num_workers=2)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                         shuffle=False, num_workers=2)
                                         
 
 if args.task == 'mae':
+    if args.data_augmentation:
+        trainset = torch.load(os.path.join(root_folder,'train_dataset_augmented.pt'))
+    else:
+        trainset = torch.load(os.path.join(root_folder,'train_dataset.pt'))
+    testset = torch.load(os.path.join(root_folder, 'dev_dataset.pt'))
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                            shuffle=True, num_workers=2)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                            shuffle=False, num_workers=2)
     # Initilize MAE model
     model = MaskedAutoEncoder(
         Transformer(embedding_dim=args.encoder_dim, n_heads=args.n_heads, n_layers=args.n_layers, feedforward_dim=args.ff_dim),
@@ -77,6 +77,8 @@ if args.task == 'mae':
     )
     # Move the model to GPU
     model.to(torch_device)
+    model.train()
+
     # Create optimizer
 
     # You may want to tune these hyperparameters to get better performance
@@ -117,6 +119,16 @@ if args.task == 'mae':
     print('plot has been saved to mae_losses.png')
 
 elif args.task == 'seg':
+    if args.data_augmentation:
+        trainset = torch.load(os.path.join(root_folder,'train_dataset_augmented.pt'))
+    else:
+        trainset = torch.load(os.path.join(root_folder,'train_dataset.pt'))
+    testset = torch.load(os.path.join(root_folder, 'dev_dataset.pt'))
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                            shuffle=True, num_workers=2)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                            shuffle=False, num_workers=2)
+
     mae = MaskedAutoEncoder(
         Transformer(embedding_dim=args.encoder_dim, n_heads=args.n_heads, n_layers=args.n_layers, feedforward_dim=args.ff_dim),
         Transformer(embedding_dim=args.decoder_dim, n_heads=args.n_heads, n_layers=args.n_layers, feedforward_dim=args.ff_dim),
@@ -134,6 +146,7 @@ elif args.task == 'seg':
     # if args.load_pretraining:
     #     model.load_state_dict(torch.load(os.path.join(root_folder, "seg_best.pt")))
     model.to(torch_device)
+    model.train()
 
     # You may want to tune these hyperparameters to get better performance
     optimizer = optim.AdamW(model.parameters(), lr=args.lr, betas=(0.9, 0.95), weight_decay=1e-4)
@@ -149,8 +162,10 @@ elif args.task == 'seg':
     epoch_iterator = trange(num_epochs)
     for epoch in epoch_iterator:
         # Train
+        model.train()
         if epoch == num_epochs // 2:
             optimizer = optim.AdamW(model.parameters(), lr=args.lr, betas=(0.9, 0.98), weight_decay=1e-5)
+            print('changing optimizer')
         data_iterator = tqdm(trainloader)
         for x, y in data_iterator:
             total_steps += 1
@@ -180,8 +195,6 @@ elif args.task == 'seg':
             iou = ((logits > 0) & (y == 1)).count_nonzero() / ((logits > 0) | (y == 1)).count_nonzero()
             dice = 2 * iou / (iou + 1)
             val_dice.append(dice.item())
-
-        model.train()
 
         all_val_dice.append(np.mean(val_dice))
 
@@ -224,6 +237,16 @@ elif args.task == 'seg':
 
 
 elif args.task == 'reg':
+    if args.data_augmentation:
+        trainset = torch.load(os.path.join(root_folder,'train_dataset_regression.pt'))
+    else:
+        trainset = torch.load(os.path.join(root_folder,'train_dataset.pt'))
+    testset = torch.load(os.path.join(root_folder, 'dev_dataset.pt'))
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                            shuffle=True, num_workers=2)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                            shuffle=False, num_workers=2)
+
     mae = MaskedAutoEncoder(
         Transformer(embedding_dim=args.encoder_dim, n_heads=args.n_heads, n_layers=args.n_layers, feedforward_dim=args.ff_dim),
         Transformer(embedding_dim=args.decoder_dim, n_heads=args.n_heads, n_layers=args.n_layers, feedforward_dim=args.ff_dim),
@@ -245,14 +268,15 @@ elif args.task == 'reg':
     total_steps = 0
     num_epochs = args.num_epoches
     train_logfreq = 100
-    losses = []
-    train_dice = []
-    all_val_dice = []
-    best_val_dice = 0
+    losses_reg = []
+    losses_class = []
+    val_losses_reg = []
+    val_accuracy = []
 
     epoch_iterator = trange(num_epochs)
     for epoch in epoch_iterator:
         # Train
+        model.train()
         if epoch == num_epochs // 2:
             optimizer = optim.AdamW(model.parameters(), lr=args.lr, betas=(0.9, 0.98), weight_decay=1e-5)
             print('changing optimizer')
@@ -266,65 +290,57 @@ elif args.task == 'reg':
             loss_reg = torch.mean(F.mse_loss(output_reg, y_reg))
             loss_class = torch.mean(F.binary_cross_entropy_with_logits(output_class, y_class))
             loss = (loss_reg + loss_class) / 2
-            iou = ((logits > 0) & (y == 1)).count_nonzero() / ((logits > 0) | (y == 1)).count_nonzero()
-            dice = 2 * iou / (iou + 1)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            data_iterator.set_postfix(loss=loss.item(), train_dice=dice.item())
+            data_iterator.set_postfix(loss=loss.item())
 
             if total_steps % train_logfreq == 0:
-                losses.append(loss.item())
-                train_dice.append(dice.item())
+                losses_reg.append(loss.item())
+                losses_class.append(loss.item())
 
         # Validation
-        val_dice = []
+        losses_reg = []
+        losses_class = []
+        accuracies = []
         model.eval()
         for x, y in testloader:
             x, y = x.to(torch_device).float(), y.to(torch_device).float()
             with torch.no_grad():
-                logits = model(x)
-            iou = ((logits > 0) & (y == 1)).count_nonzero() / ((logits > 0) | (y == 1)).count_nonzero()
-            dice = 2 * iou / (iou + 1)
-            val_dice.append(dice.item())
+                output_reg, output_class = model(x)
+            loss_reg = torch.mean(F.mse_loss(output_reg, y_reg))
+            accuracy = torch.mean(((output_class > 0).bool() == y_class.bool()).float())
+            losses_reg.append(loss_reg)
+            accuracies.append(accuracy)
 
-        model.train()
-
-        all_val_dice.append(np.mean(val_dice))
+        losses_reg = sum(losses_reg) / len(losses_reg)
+        losses_class = sum(losses_class) / len(losses_class)
+        accuracies = sum(accuracies) / len(accuracies)
+        val_losses_reg.append(losses_reg)
+        val_accuracy.append(accuracies.tolist())
 
         # Save best model
-        if np.mean(val_dice) > best_val_dice:
-            best_val_dice = np.mean(val_dice)
-            torch.save(model.state_dict(), os.path.join(root_folder, "seg_best.pt"))
 
-        epoch_iterator.set_postfix(val_dice=np.mean(val_dice), best_val_dice=best_val_dice)
+        epoch_iterator.set_postfix(loss_reg =losses_reg.tolist(), accuracy=accuracies.tolist())
+    losses_class = []
+    val_losses_reg = []
+    val_accuracy = []
 
-    if args.load_pretraining:
-        plt.plot(losses)
-        plt.title('Linear Classification Train Loss')
-        plt.savefig(os.path.join(args.exp_name, 'seg_losses'))
-        plt.clf()
-        plt.plot(train_dice)
-        plt.title('Linear Classification Train dice')
-        plt.savefig(os.path.join(args.exp_name, 'seg_train_dice'))
-        plt.clf()
-        plt.plot(all_val_dice)
-        plt.title('Linear Classification Val dice')
-        plt.savefig(os.path.join(args.exp_name, 'seg_val_dice'))
-        plt.clf()
-        print('plots have been saved to seg_losses.png, seg_train_dice.png and seg_val_dice.png')
-    else:
-        plt.plot(losses)
-        plt.title('Linear Classification Train Loss')
-        plt.savefig(os.path.join(args.exp_name, 'seg_losses_nopt'))
-        plt.clf()
-        plt.plot(train_dice)
-        plt.title('Linear Classification Train dice')
-        plt.savefig(os.path.join(args.exp_name, 'seg_train_dice_nopt'))
-        plt.clf()
-        plt.plot(all_val_dice)
-        plt.title('Linear Classification Val dice')
-        plt.savefig(os.path.join(args.exp_name, 'seg_val_dice_nopt'))
-        plt.clf()
-        print('plots have been saved to seg_losses_nopt.png, seg_train_dice_nopt.png and seg_val_dice_nopt.png')
+    plt.plot(losses_reg)
+    plt.title('Regression losses for training')
+    plt.savefig(os.path.join(args.exp_name, 'reg_loss_train'))
+    plt.clf()
+    plt.plot(losses_class)
+    plt.title('Classification losses for training')
+    plt.savefig(os.path.join(args.exp_name, 'class_loss_train'))
+    plt.clf()
+    plt.plot(val_losses_reg)
+    plt.title('Validation MSE for regression')
+    plt.savefig(os.path.join(args.exp_name, 'reg_mse_val'))
+    plt.clf()
+    plt.plot(val_accuracy)
+    plt.title('Validation accuracy for bifurcation checking')
+    plt.savefig(os.path.join(args.exp_name, 'reg_acc_val'))
+    plt.clf()
+    print('plots have been saved')
--- run_seg.sh
+++ run_seg.sh
@@ -1,18 +1,30 @@
 #!/usr/bin/env bash
 
-python main.py --task seg --batch_size 128 --lr 1e-4 --num_epoches 200 \
-    --n_layers 6 --n_heads 4 --ff_dim 1024 --encoder_dim 512 --decoder_dim 512 \
-    --exp_name bs128_lr1e-4_layer6_head4_ff1024_enc512_dec512_aug --gpu_id 0 #--load_pretraining
+python main.py --task reg --batch_size 128 --lr 1e-4 --num_epoches 400 \
+    --n_layers 4 --n_heads 4 --ff_dim 1024 --encoder_dim 256 --decoder_dim 256\
+    --exp_name regression --gpu_id 2 #--load_pretraining
 
-# python main.py --task seg --batch_size 128 --lr 1e-4 --num_epoches 200 \
+# python main.py --task seg --batch_size 128 --lr 1e-4 --num_epoches 400 \
+#     --n_layers 6 --n_heads 4 --ff_dim 1024 --encoder_dim 512 --decoder_dim 512 \
+#     --exp_name bs128_lr1e-4_layer6_head4_ff1024_enc512_dec512 --gpu_id 0 #--load_pretraining
+
+# python main.py --task seg --batch_size 128 --lr 1e-4 --num_epoches 400 \
 #     --n_layers 6 --n_heads 4 --ff_dim 2048 --encoder_dim 512 --decoder_dim 512 \
-#     --exp_name bs128_lr1e-4_layer6_head4_ff2048_enc512_dec512_aug --gpu_id 1 --load_pretraining
+#     --exp_name bs128_lr1e-4_layer6_head4_ff2048_enc512_dec512 --gpu_id 1 #--load_pretraining
 
-# python main.py --task seg --batch_size 128 --lr 1e-4 --num_epoches 200 \
+# python main.py --task seg --batch_size 128 --lr 1e-4 --num_epoches 400 \
 #     --n_layers 6 --n_heads 8 --ff_dim 1024 --encoder_dim 512 --decoder_dim 512 \
-#     --exp_name bs128_lr1e-4_layer6_head8_ff1024_enc512_dec512_aug --gpu_id 2 #--load_pretraining
+#     --exp_name bs128_lr1e-4_layer6_head8_ff1024_enc512_dec512 --gpu_id 2 #--load_pretraining
 
-# python main.py --task seg --batch_size 128 --lr 1e-4 --num_epoches 200 \
+# python main.py --task seg --batch_size 128 --lr 1e-4 --num_epoches 400 \
 #     --n_layers 4 --n_heads 4 --ff_dim 1024 --encoder_dim 256 --decoder_dim 256 \
+#     --exp_name bs128_lr1e-4_layer4_head4_ff1024_enc256_dec256 --gpu_id 3 #--load_pretraining
+
+# python main.py --task seg --batch_size 128 --lr 1e-4 --num_epoches 400 \
+#     --n_layers 6 --n_heads 4 --ff_dim 2048 --encoder_dim 512 --decoder_dim 512 --data_augmentation\
+#     --exp_name bs128_lr1e-4_layer6_head4_ff2048_enc512_dec512_aug --gpu_id 1 #--load_pretraining
+
+# python main.py --task seg --batch_size 128 --lr 1e-4 --num_epoches 400 \
+#     --n_layers 4 --n_heads 4 --ff_dim 1024 --encoder_dim 256 --decoder_dim 256 --data_augmentation\
 #     --exp_name bs128_lr1e-4_layer4_head4_ff1024_enc256_dec256_aug --gpu_id 3 #--load_pretraining
 
